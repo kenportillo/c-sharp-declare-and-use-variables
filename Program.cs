8ebc0945120b4aee06b5c0c239e2c21dd80ba0c4
github username <github email address>
2020-02-17 15:05:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Declare_and_use_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            string aFriend = "Bill";
+            // Console.WriteLine("Hello " + aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+            aFriend = "Maria";
+            Console.WriteLine(aFriend);
+        }
+    }
+}
